4d555346f34e0dc37e12f82dcd0fe3b958bf6169
rajkumarkanjo <rajkumarkanjo@gmail.com>
2019-07-14 02:25:44 +0530

    Migrating from PostgreSQL to H2 database (in-memory). http://localhost:8080/h2-console

--- SpringBoot_JSP/src/main/java/com/user/model/User.java
+++ SpringBoot_JSP/src/main/java/com/user/model/User.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="userjsp")
+//@Table(name="userjsp") //as i am using H2 database
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
