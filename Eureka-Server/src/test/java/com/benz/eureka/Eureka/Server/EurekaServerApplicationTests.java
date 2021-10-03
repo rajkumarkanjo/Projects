0b637195697f045390718b4a9d56303912b121bf
RAJ <rajkumarkanjo@gmail.com>
2021-10-03 17:01:53 +0530

    Add files via upload

---
+++ Eureka-Server/src/test/java/com/benz/eureka/Eureka/Server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benz.eureka.Eureka.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
